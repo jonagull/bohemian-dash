ae816354424071e0808b5639e08e2b55fa5dc64e
Jonathan Gulliksen <jonathangullik@gmail.com>
2022-02-24 19:42:01 +0100

    Modified import files to fit new scss file names

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.scss';
+import './scss/app.scss';
+import Header from './components/Header'
 
 function App() {
   return (  
     <React.Fragment>
-      
+        <Header />
     </React.Fragment>
   ) 
 }
