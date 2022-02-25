d63354dd3b20eafe406c2645af8def899451ea5f
Jonathan Gulliksen <jonathangullik@gmail.com>
2022-02-25 13:11:07 +0100

    Changing svg's into components

---
+++ src/components/assets/Lightmode-sun.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function LightmodeSun () {
+    return (
+        <>
+            <svg width="84" height="85" viewBox="0 0 84 85" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_22_55)">
+            <path d="M42.1756 54.1496C48.6158 54.1496 53.8367 48.9288 53.8367 42.4885C53.8367 36.0483 48.6158 30.8275 42.1756 30.8275C35.7354 30.8275 30.5145 36.0483 30.5145 42.4885C30.5145 48.9288 35.7354 54.1496 42.1756 54.1496Z" stroke="white" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M42.1756 20.1411V13.2488" stroke="white" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M26.3722 26.6852L21.499 21.8119" stroke="white" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M19.8281 42.4885H12.9359" stroke="white" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M26.3722 58.2919L21.499 63.1652" stroke="white" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M42.1756 64.836V71.7282" stroke="white" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M57.9789 58.2919L62.8522 63.1652" stroke="white" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M64.5231 42.4885H71.4153" stroke="white" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M57.9789 26.6852L62.8522 21.8119" stroke="white" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
+            </g>
+            <defs>
+            <clipPath id="clip0_22_55">
+            <rect x="10.8474" y="11.1603" width="62.6565" height="62.6565" rx="5" fill="white"/>
+            </clipPath>
+            </defs>
+            </svg>
+        </>
+    )
+}
+
+
