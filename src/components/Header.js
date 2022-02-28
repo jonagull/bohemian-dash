cfbaa9dccb782b6054ef4815af1628629a997052
Jonathan Gulliksen <jonathangullik@gmail.com>
2022-02-28 12:26:52 +0100

    we are now using BEM standard css naming

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,14 @@
-import React from 'react'
-import logo from './assets/bhd-logo.svg'
-
-
+import React from "react";
+import logo from "./assets/bhd-logo.svg";
 
 export default function Header() {
-    return(
-        <React.Fragment>
-            <div className="header--container">
-                <div className="logo--container">
-                    <img src={logo}></img>
-                </div>
-            </div>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <header className="header">
+        <div className="header__logo">
+          <img src={logo}></img>
+        </div>
+      </header>
+    </React.Fragment>
+  );
 }
-
