d94e85929eb778b8df9d1c2b5d5fb42992f91444
Jonathan Gulliksen <jonathangullik@gmail.com>
2022-02-28 12:41:19 +0100

    formatting entire project with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import './scss/app.scss';
-import Header from './components/Header'
+import React from "react";
+import "./scss/app.scss";
+import Header from "./components/Header";
 
 function App() {
-  return (  
+  return (
     <React.Fragment>
-        <Header />
+      <Header />
     </React.Fragment>
-  ) 
+  );
 }
 
 export default App;
