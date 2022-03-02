65fb1a03d05e92767cd3090bbc02bcdcd9415dcc
Jonathan Gulliksen <jonathangullik@gmail.com>
2022-03-02 15:46:24 +0100

    Initing StockWidget with basic styles

---
+++ src/components/widgets/StockWidget.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function StockWidget() {
+  return (
+    <React.Fragment>
+      <div className="stock-widget__wrapper">ApesTogetherStrong</div>
+    </React.Fragment>
+  );
+}
