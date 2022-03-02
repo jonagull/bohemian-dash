f83b686a6af4a50e8992b9f13cdbb644c44fb483
Jonathan Gulliksen <jonathangullik@gmail.com>
2022-03-02 17:38:56 +0100

    Added dummy data and basic styling

--- src/components/widgets/StockWidget.tsx
+++ src/components/widgets/StockWidget.tsx
@@ -3,7 +3,19 @@ import React from "react";
 export function StockWidget() {
   return (
     <React.Fragment>
-      <div className="stock-widget__wrapper">ApesTogetherStrong</div>
+      <div className="stock-widget__wrapper">
+        <div className="ticker__container">
+          <h1>AAPL</h1>
+          <p>+0.58%</p>
+        </div>
+        <div className="company-name__container">
+          <p>Apple Corporation</p>
+        </div>
+        <div className="chart-data__container">Chart data</div>
+        <div className="price__container">
+          <h1>114.44</h1>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
