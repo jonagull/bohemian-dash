2832d79c581f441a9c9b691dbe139079e75d8ab3
Jonathan Gulliksen <jonathangullik@gmail.com>
2022-03-02 15:06:04 +0100

    Mapping Logo's and making code nice and pretty for mentor

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,86 @@
-import LightmodeSun from "./components/assets/Lightmode-sun";
+import { ReactComponent as Logo01d } from "./assets/icons/01d.svg";
+import { ReactComponent as Logo01m } from "./assets/icons/01m.svg";
+import { ReactComponent as Logo01n } from "./assets/icons/01n.svg";
+import { ReactComponent as Logo02d } from "./assets/icons/02d.svg";
+import { ReactComponent as Logo02m } from "./assets/icons/02m.svg";
+import { ReactComponent as Logo02n } from "./assets/icons/02n.svg";
+import { ReactComponent as Logo03d } from "./assets/icons/03d.svg";
+import { ReactComponent as Logo03m } from "./assets/icons/03m.svg";
+import { ReactComponent as Logo03n } from "./assets/icons/03n.svg";
+import { ReactComponent as Logo04 } from "./assets/icons/04.svg";
+import { ReactComponent as Logo05d } from "./assets/icons/05d.svg";
+import { ReactComponent as Logo05m } from "./assets/icons/05m.svg";
+import { ReactComponent as Logo05n } from "./assets/icons/05n.svg";
+import { ReactComponent as Logo06d } from "./assets/icons/06d.svg";
+import { ReactComponent as Logo06m } from "./assets/icons/06m.svg";
+import { ReactComponent as Logo06n } from "./assets/icons/06n.svg";
+import { ReactComponent as Logo07d } from "./assets/icons/07d.svg";
+import { ReactComponent as Logo07m } from "./assets/icons/07m.svg";
+import { ReactComponent as Logo07n } from "./assets/icons/07n.svg";
+import { ReactComponent as Logo08d } from "./assets/icons/08d.svg";
+import { ReactComponent as Logo08m } from "./assets/icons/08m.svg";
+import { ReactComponent as Logo08n } from "./assets/icons/08n.svg";
+import { ReactComponent as Logo09 } from "./assets/icons/09.svg";
+import { ReactComponent as Logo10 } from "./assets/icons/10.svg";
+import { ReactComponent as Logo11 } from "./assets/icons/11.svg";
+import { ReactComponent as Logo12 } from "./assets/icons/12.svg";
+import { ReactComponent as Logo13 } from "./assets/icons/13.svg";
+import { ReactComponent as Logo14 } from "./assets/icons/14.svg";
+import { ReactComponent as Logo15 } from "./assets/icons/15.svg";
+import { ReactComponent as Logo20d } from "./assets/icons/20d.svg";
+import { ReactComponent as Logo20m } from "./assets/icons/20m.svg";
+import { ReactComponent as Logo20n } from "./assets/icons/20n.svg";
+import { ReactComponent as Logo21d } from "./assets/icons/21d.svg";
+import { ReactComponent as Logo21m } from "./assets/icons/21m.svg";
+import { ReactComponent as Logo21n } from "./assets/icons/21n.svg";
+import { ReactComponent as Logo22 } from "./assets/icons/22.svg";
+import { ReactComponent as Logo23 } from "./assets/icons/23.svg";
+import { ReactComponent as Logo24d } from "./assets/icons/24d.svg";
+import { ReactComponent as Logo24m } from "./assets/icons/24m.svg";
+import { ReactComponent as Logo24n } from "./assets/icons/24n.svg";
+import { ReactComponent as Logo25d } from "./assets/icons/25d.svg";
+import { ReactComponent as Logo25m } from "./assets/icons/25m.svg";
+import { ReactComponent as Logo25n } from "./assets/icons/25n.svg";
+import { ReactComponent as Logo26d } from "./assets/icons/26d.svg";
+import { ReactComponent as Logo26m } from "./assets/icons/26m.svg";
+import { ReactComponent as Logo26n } from "./assets/icons/26n.svg";
+import { ReactComponent as Logo27d } from "./assets/icons/27d.svg";
+import { ReactComponent as Logo27m } from "./assets/icons/27m.svg";
+import { ReactComponent as Logo27n } from "./assets/icons/27n.svg";
+import { ReactComponent as Logo28d } from "./assets/icons/28d.svg";
+import { ReactComponent as Logo28m } from "./assets/icons/28m.svg";
+import { ReactComponent as Logo28n } from "./assets/icons/28n.svg";
+import { ReactComponent as Logo29d } from "./assets/icons/29d.svg";
+import { ReactComponent as Logo29m } from "./assets/icons/29m.svg";
+import { ReactComponent as Logo29n } from "./assets/icons/29n.svg";
+import { ReactComponent as Logo30 } from "./assets/icons/30.svg";
+import { ReactComponent as Logo31 } from "./assets/icons/31.svg";
+import { ReactComponent as Logo32 } from "./assets/icons/32.svg";
+import { ReactComponent as Logo33 } from "./assets/icons/33.svg";
+import { ReactComponent as Logo34 } from "./assets/icons/34.svg";
+import { ReactComponent as Logo40d } from "./assets/icons/40d.svg";
+import { ReactComponent as Logo40m } from "./assets/icons/40m.svg";
+import { ReactComponent as Logo40n } from "./assets/icons/40n.svg";
+import { ReactComponent as Logo41d } from "./assets/icons/41d.svg";
+import { ReactComponent as Logo41m } from "./assets/icons/41m.svg";
+import { ReactComponent as Logo41n } from "./assets/icons/41n.svg";
+import { ReactComponent as Logo42d } from "./assets/icons/42d.svg";
+import { ReactComponent as Logo42m } from "./assets/icons/42m.svg";
+import { ReactComponent as Logo42n } from "./assets/icons/42n.svg";
+import { ReactComponent as Logo43d } from "./assets/icons/43d.svg";
+import { ReactComponent as Logo43m } from "./assets/icons/43m.svg";
+import { ReactComponent as Logo43n } from "./assets/icons/43n.svg";
+import { ReactComponent as Logo44d } from "./assets/icons/44d.svg";
+import { ReactComponent as Logo44m } from "./assets/icons/44m.svg";
+import { ReactComponent as Logo44n } from "./assets/icons/44n.svg";
+import { ReactComponent as Logo45d } from "./assets/icons/45d.svg";
+import { ReactComponent as Logo45m } from "./assets/icons/45m.svg";
+import { ReactComponent as Logo45n } from "./assets/icons/45n.svg";
+import { ReactComponent as Logo46 } from "./assets/icons/46.svg";
+import { ReactComponent as Logo47 } from "./assets/icons/47.svg";
+import { ReactComponent as Logo48 } from "./assets/icons/48.svg";
+import { ReactComponent as Logo49 } from "./assets/icons/49.svg";
+import { ReactComponent as Logo50 } from "./assets/icons/50.svg";
 
 export const LocalStorageKeys = {
   latitude: "bhd_latitude",
@@ -6,89 +88,173 @@ export const LocalStorageKeys = {
 };
 
 export enum WeatherIcon {
-  clearSky = "clearsky",
+  clearSkyDay = "clearsky_day",
+  clearSkyNight = "clearsky_night",
+  clearSkyPolartwilight = "clearsky_polartwilight",
   cloudy = "cloudy",
-  fair = "fair",
+  fairDay = "fair_day",
+  fairNight = "fair_night",
+  fairPolartwilight = "fair_polartwilight",
   fog = "fog",
   heavyRain = "heavyrain",
   heavyRainAndThunder = "heavyrainandthunder",
-  heavyRainShowers = "heavyrainshowers",
-  heavyRainShowersAndThunder = "heavyrainshowersandthunder",
+  heavyRainShowersDay = "heavyrainshowers_day",
+  heavyRainShowersNight = "heavyrainshowers_nigth",
+  heavyRainShowersPolartwilight = "heavyrainshowers_polartwilight",
+  heavyRainShowersAndThunderDay = "heavyrainshowersandthunder_day",
+  heavyRainShowersAndThunderNight = "heavyrainshowersandthunder_night",
+  heavyRainShowersAndThunderPolartwilight = "heavyrainshowersandthunder_polartwilight",
   heavySleet = "heavysleet",
   heavySleetAndThunder = "heavysleetandthunder",
-  heavySleetShowers = "heavysleetshowers",
-  heavySleetShowersAndThunder = "heavysleetshowersandthunder",
+  heavySleetShowersDay = "heavysleetshowers_day",
+  heavySleetShowersNight = "heavysleetshowers_night",
+  heavySleetShowersPolartwilight = "heavysleetshowers_polartwilight",
+  heavySleetShowersAndThunderDay = "heavysleetshowersandthunder_day",
+  heavySleetShowersAndThunderNight = "heavysleetshowersandthunder_night",
+  heavySleetShowersAndThunderPolartwilight = "heavysleetshowersandthunder_polartwilight",
   heavySnow = "heavysnow",
   heavySnowAndThunder = "heavysnowandthunder",
-  heavySnowShowers = "heavysnowshowers",
-  heavySnowShowersAndThunder = "heavysnowshowersandthunder",
+  heavySnowShowersDay = "heavysnowshowers_day",
+  heavySnowShowersNight = "heavysnowshowers_night",
+  heavySnowShowersPolartwilight = "heavysnowshowers_polartwilight",
+  heavySnowShowersAndThunderDay = "heavysnowshowersandthunder_day",
+  heavySnowShowersAndThunderNight = "heavysnowshowersandthunder_night",
+  heavySnowShowersAndThunderPolartwilight = "heavysnowshowersandthunder_polartwilight",
   lightRain = "lightrain",
   lightRainAndThunder = "lightrainandthunder",
-  lightRainShowers = "lightrainshowers",
-  lightRainShowersAndThunder = "lightrainshowersandthunder",
+  lightRainShowersDay = "lightrainshowers_day",
+  lightRainShowersNight = "lightrainshowers_night",
+  lightRainShowersPolartwilight = "lightrainshowers_polartwilight",
+  lightRainShowersAndThunderDay = "lightrainshowersandthunder_day",
+  lightRainShowersAndThunderNight = "lightrainshowersandthunder_night",
+  lightRainShowersAndThunderPolartwilight = "lightrainshowersandthunder_polartwilight",
   lightSleet = "lightsleet",
   lightSleetAndThunder = "lightsleetandthunder",
-  lightSleetShowers = "lightsleetshowers",
+  lightSleetShowersDay = "lightsleetshowers_day",
+  lightSleetShowersNight = "lightsleetshowers_night",
+  lightSleetShowersPolartwilight = "lightsleetshowers_polartwilight",
   lightSnow = "lightsnow",
   lightSnowAndThunder = "lightsnowandthunder",
-  lightSnowShowers = "lightsnowshowers",
-  lightSleetShowersAndThunder = "lightssleetshowersandthunder",
-  lightSnowShowersAndThunder = "lightssnowshowersandthunder",
-  partlyCloudy = "partlycloudy",
+  lightSnowShowersDay = "lightsnowshowers_day",
+  lightSnowShowersNight = "lightsnowshowers_night",
+  lightSnowShowersPolartwilight = "lightsnowshowers_polartwilight",
+  lightSleetShowersAndThunderDay = "lightssleetshowersandthunder_day",
+  lightSleetShowersAndThunderNight = "lightssleetshowersandthunder_night",
+  lightSleetShowersAndThunderPolartwilight = "lightssleetshowersandthunder_polartwilight",
+  lightSnowShowersAndThunderDay = "lightssnowshowersandthunder_day",
+  lightSnowShowersAndThunderNight = "lightssnowshowersandthunder_night",
+  lightSnowShowersAndThunderPolartwilight = "lightssnowshowersandthunder_polartwilight",
+  partlyCloudyDay = "partlycloudy_day",
+  partlyCloudyNight = "partlycloudy_night",
+  partlyCloudyPolartwilight = "partlycloudy_polartwilight",
   rain = "rain",
   rainAndThunder = "rainandthunder",
-  rainShowers = "rainshowers",
-  rainShowersAndThunder = "rainshowersandthunder",
+  rainShowersDay = "rainshowers_day",
+  rainShowersNight = "rainshowers_night",
+  rainShowersPolartwilight = "rainshowers_polartwilight",
+  rainShowersAndThunderDay = "rainshowersandthunder_day",
+  rainShowersAndThunderNight = "rainshowersandthunder_night",
+  rainShowersAndThunderPolartwilight = "rainshowersandthunder_polartwilight",
   sleet = "sleet",
   sleetAndThunder = "sleetandthunder",
-  sleetShowers = "sleetshowers",
-  sleetShowersAndThunder = "sleetshowersandthunder",
+  sleetShowersDay = "sleetshowers_day",
+  sleetShowersNight = "sleetshowers_night",
+  sleetShowersPolartwilight = "sleetshowers_polartwilight",
+  sleetShowersAndThunderDay = "sleetshowersandthunder_day",
+  sleetShowersAndThunderNight = "sleetshowersandthunder_night",
+  sleetShowersAndThunderPolartwilight = "sleetshowersandthunder_polartwilight",
   snow = "snow",
   snowAndThunder = "snowandthunder",
-  snowShowers = "snowshowers",
-  snowShowersAndThunder = "snowshowersandthunder",
+  snowShowersDay = "snowshowers_day",
+  snowShowersNight = "snowshowers_night",
+  snowShowersPolartwilight = "snowshowers_polartwilight",
+  snowShowersAndThunderDay = "snowshowersandthunder_day",
+  snowShowersAndThunderNight = "snowshowersandthunder_night",
+  snowShowersAndThunderPolartwilight = "snowshowersandthunder_polartwilight",
 }
 
 export const weatherIcons = {
-  [WeatherIcon.clearSky]: LightmodeSun,
-  [WeatherIcon.cloudy]: LightmodeSun,
-  [WeatherIcon.fair]: LightmodeSun,
-  [WeatherIcon.fog]: LightmodeSun,
-  [WeatherIcon.heavyRain]: LightmodeSun,
-  [WeatherIcon.heavyRainAndThunder]: LightmodeSun,
-  [WeatherIcon.heavyRainShowers]: LightmodeSun,
-  [WeatherIcon.heavyRainShowersAndThunder]: LightmodeSun,
-  [WeatherIcon.heavySleet]: LightmodeSun,
-  [WeatherIcon.heavySleetAndThunder]: LightmodeSun,
-  [WeatherIcon.heavySleetShowers]: LightmodeSun,
-  [WeatherIcon.heavySleetShowersAndThunder]: LightmodeSun,
-  [WeatherIcon.heavySnow]: LightmodeSun,
-  [WeatherIcon.heavySnowAndThunder]: LightmodeSun,
-  [WeatherIcon.heavySnowShowers]: LightmodeSun,
-  [WeatherIcon.heavySnowShowersAndThunder]: LightmodeSun,
-  [WeatherIcon.lightRain]: LightmodeSun,
-  [WeatherIcon.lightRainAndThunder]: LightmodeSun,
-  [WeatherIcon.lightRainShowers]: LightmodeSun,
-  [WeatherIcon.lightRainShowersAndThunder]: LightmodeSun,
-  [WeatherIcon.lightSleet]: LightmodeSun,
-  [WeatherIcon.lightSleetAndThunder]: LightmodeSun,
-  [WeatherIcon.lightSleetShowers]: LightmodeSun,
-  [WeatherIcon.lightSnow]: LightmodeSun,
-  [WeatherIcon.lightSnowAndThunder]: LightmodeSun,
-  [WeatherIcon.lightSnowShowers]: LightmodeSun,
-  [WeatherIcon.lightSleetShowersAndThunder]: LightmodeSun,
-  [WeatherIcon.lightSnowShowersAndThunder]: LightmodeSun,
-  [WeatherIcon.partlyCloudy]: LightmodeSun,
-  [WeatherIcon.rain]: LightmodeSun,
-  [WeatherIcon.rainAndThunder]: LightmodeSun,
-  [WeatherIcon.rainShowers]: LightmodeSun,
-  [WeatherIcon.rainShowersAndThunder]: LightmodeSun,
-  [WeatherIcon.sleet]: LightmodeSun,
-  [WeatherIcon.sleetAndThunder]: LightmodeSun,
-  [WeatherIcon.sleetShowers]: LightmodeSun,
-  [WeatherIcon.sleetShowersAndThunder]: LightmodeSun,
-  [WeatherIcon.snow]: LightmodeSun,
-  [WeatherIcon.snowAndThunder]: LightmodeSun,
-  [WeatherIcon.snowShowers]: LightmodeSun,
-  [WeatherIcon.snowShowersAndThunder]: LightmodeSun,
+  [WeatherIcon.clearSkyDay]: Logo01d,
+  [WeatherIcon.clearSkyNight]: Logo01n,
+  [WeatherIcon.clearSkyPolartwilight]: Logo01m,
+  [WeatherIcon.cloudy]: Logo04,
+  [WeatherIcon.fairDay]: Logo02d,
+  [WeatherIcon.fairNight]: Logo02n,
+  [WeatherIcon.fairPolartwilight]: Logo02m,
+  [WeatherIcon.fog]: Logo15,
+  [WeatherIcon.heavyRain]: Logo10,
+  [WeatherIcon.heavyRainAndThunder]: Logo11,
+  [WeatherIcon.heavyRainShowersDay]: Logo41d,
+  [WeatherIcon.heavyRainShowersNight]: Logo41n,
+  [WeatherIcon.heavyRainShowersPolartwilight]: Logo41m,
+  [WeatherIcon.heavyRainShowersAndThunderDay]: Logo25d,
+  [WeatherIcon.heavyRainShowersAndThunderNight]: Logo25n,
+  [WeatherIcon.heavyRainShowersAndThunderPolartwilight]: Logo25m,
+  [WeatherIcon.heavySleet]: Logo48,
+  [WeatherIcon.heavySleetAndThunder]: Logo32,
+  [WeatherIcon.heavySleetShowersDay]: Logo43d,
+  [WeatherIcon.heavySleetShowersNight]: Logo43n,
+  [WeatherIcon.heavySleetShowersPolartwilight]: Logo43m,
+  [WeatherIcon.heavySleetShowersAndThunderDay]: Logo27d,
+  [WeatherIcon.heavySleetShowersAndThunderNight]: Logo27n,
+  [WeatherIcon.heavySleetShowersAndThunderPolartwilight]: Logo27m,
+  [WeatherIcon.heavySnow]: Logo50,
+  [WeatherIcon.heavySnowAndThunder]: Logo34,
+  [WeatherIcon.heavySnowShowersDay]: Logo45d,
+  [WeatherIcon.heavySnowShowersNight]: Logo45n,
+  [WeatherIcon.heavySnowShowersPolartwilight]: Logo45m,
+  [WeatherIcon.heavySnowShowersAndThunderDay]: Logo29d,
+  [WeatherIcon.heavySnowShowersAndThunderNight]: Logo29n,
+  [WeatherIcon.heavySnowShowersAndThunderPolartwilight]: Logo29m,
+  [WeatherIcon.lightRain]: Logo46,
+  [WeatherIcon.lightRainAndThunder]: Logo30,
+  [WeatherIcon.lightRainShowersDay]: Logo40d,
+  [WeatherIcon.lightRainShowersNight]: Logo40n,
+  [WeatherIcon.lightRainShowersPolartwilight]: Logo40m,
+  [WeatherIcon.lightRainShowersAndThunderDay]: Logo24d,
+  [WeatherIcon.lightRainShowersAndThunderNight]: Logo24n,
+  [WeatherIcon.lightRainShowersAndThunderPolartwilight]: Logo24m,
+  [WeatherIcon.lightSleet]: Logo47,
+  [WeatherIcon.lightSleetAndThunder]: Logo31,
+  [WeatherIcon.lightSleetShowersDay]: Logo42d,
+  [WeatherIcon.lightSleetShowersNight]: Logo42n,
+  [WeatherIcon.lightSleetShowersPolartwilight]: Logo42m,
+  [WeatherIcon.lightSnow]: Logo49,
+  [WeatherIcon.lightSnowAndThunder]: Logo33,
+  [WeatherIcon.lightSnowShowersDay]: Logo44d,
+  [WeatherIcon.lightSnowShowersNight]: Logo44n,
+  [WeatherIcon.lightSnowShowersPolartwilight]: Logo44m,
+  [WeatherIcon.lightSleetShowersAndThunderDay]: Logo26d,
+  [WeatherIcon.lightSleetShowersAndThunderNight]: Logo26n,
+  [WeatherIcon.lightSleetShowersAndThunderPolartwilight]: Logo26m,
+  [WeatherIcon.lightSnowShowersAndThunderDay]: Logo28d,
+  [WeatherIcon.lightSnowShowersAndThunderNight]: Logo28n,
+  [WeatherIcon.lightSnowShowersAndThunderPolartwilight]: Logo28m,
+  [WeatherIcon.partlyCloudyDay]: Logo03d,
+  [WeatherIcon.partlyCloudyNight]: Logo03n,
+  [WeatherIcon.partlyCloudyPolartwilight]: Logo03m,
+  [WeatherIcon.rain]: Logo09,
+  [WeatherIcon.rainAndThunder]: Logo22,
+  [WeatherIcon.rainShowersDay]: Logo05d,
+  [WeatherIcon.rainShowersNight]: Logo05n,
+  [WeatherIcon.rainShowersPolartwilight]: Logo05m,
+  [WeatherIcon.rainShowersAndThunderDay]: Logo06d,
+  [WeatherIcon.rainShowersAndThunderNight]: Logo06n,
+  [WeatherIcon.rainShowersAndThunderPolartwilight]: Logo06m,
+  [WeatherIcon.sleet]: Logo12,
+  [WeatherIcon.sleetAndThunder]: Logo23,
+  [WeatherIcon.sleetShowersDay]: Logo07d,
+  [WeatherIcon.sleetShowersNight]: Logo07n,
+  [WeatherIcon.sleetShowersPolartwilight]: Logo07m,
+  [WeatherIcon.sleetShowersAndThunderDay]: Logo20d,
+  [WeatherIcon.sleetShowersAndThunderNight]: Logo20n,
+  [WeatherIcon.sleetShowersAndThunderPolartwilight]: Logo20m,
+  [WeatherIcon.snow]: Logo13,
+  [WeatherIcon.snowAndThunder]: Logo14,
+  [WeatherIcon.snowShowersDay]: Logo08d,
+  [WeatherIcon.snowShowersNight]: Logo08n,
+  [WeatherIcon.snowShowersPolartwilight]: Logo08m,
+  [WeatherIcon.snowShowersAndThunderDay]: Logo21d,
+  [WeatherIcon.snowShowersAndThunderNight]: Logo21n,
+  [WeatherIcon.snowShowersAndThunderPolartwilight]: Logo21m,
 };
