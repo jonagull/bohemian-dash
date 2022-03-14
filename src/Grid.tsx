b527a8b7cfe4caa767c101c4456e24f014620e32
Jonathan Gulliksen <jonathangullik@gmail.com>
2022-03-14 17:50:07 +0100

    Created SaunaWidget with SaunaChart, rendered to Grid

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import GridLayout from "react-grid-layout";
+import { SaunaWidget } from "./components/widgets/SaunaWidget/SaunaWidget";
 import { StockWidget } from "./components/widgets/StockWidget";
 import WeatherWidget from "./components/widgets/WeatherWidget";
 
@@ -7,6 +8,7 @@ export const Grid = () => {
   const layout = [
     { i: "a", x: 0, y: 0, w: 3, h: 9 },
     { i: "b", x: 3, y: 0, w: 3, h: 9 },
+    { i: "c", x: 6, y: 0, w: 3, h: 9 },
   ];
 
   const [width, setWidth] = useState<number>(window.innerWidth);
@@ -14,8 +16,6 @@ export const Grid = () => {
   const updateInnerWidth = () => {
     setWidth(window.innerWidth);
   };
-
-  console.log(width);
 
   useEffect(() => {
     window.addEventListener("resize", updateInnerWidth, false);
@@ -34,6 +34,9 @@ export const Grid = () => {
       </div>
       <div key="b">
         <WeatherWidget />
+      </div>
+      <div key="c">
+        <SaunaWidget />
       </div>
     </GridLayout>
   );
---
+++ src/components/widgets/SaunaWidget/SaunaWidget.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useGetSaunaData } from "./useGetSaunaData";
+import { ReactComponent as HumidIcon } from "../../../assets/icons/HumidIcon.svg";
+import { ReactComponent as ThermometerIcon } from "../../../assets/icons/thermometer.svg";
+import { SaunaChart } from "./SaunaChart";
+
+export const SaunaWidget = () => {
+  const saunaData = useGetSaunaData();
+
+  return (
+    <div className="sauna-widget__wrapper">
+      <div className="title__container">
+        <h1>Sauna</h1>
+      </div>
+      <div className="sauna-chart__container">
+        <SaunaChart data={saunaData} />
+      </div>
+      <div className="sauna-metrics__container">
+        <div className="stats__container">
+          <h2 className="temperature__container">
+            <ThermometerIcon /> {saunaData && saunaData[0].temperature} C°
+          </h2>
+          <h2 className="humid__container">
+            <span>
+              <HumidIcon />
+            </span>
+            <span>{saunaData && saunaData[0].humidity} %</span>
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
