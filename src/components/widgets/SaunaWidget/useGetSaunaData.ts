e5cff1310d4f3ff3e5ce52ee4dd05e338ed826df
Jonathan Gulliksen <jonathangullik@gmail.com>
2022-03-14 17:50:54 +0100

    Created hook for fetching SaunaData

---
+++ src/components/widgets/SaunaWidget/useGetSaunaData.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { SaunaData, SaunaResponseBody } from "../../../types";
+
+export const useGetSaunaData = () => {
+  const [saunaData, setSaunaData] = useState<SaunaData[] | null>(null);
+
+  const fetchData = () => {
+    fetch("https://api.bhd.v3p2k2.com/sauna/data")
+      .then((res) => res.json())
+      .then((data: SaunaResponseBody) => {
+        setSaunaData(data.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return saunaData;
+};
