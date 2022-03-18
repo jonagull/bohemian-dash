e182fdef2b7654bbdd6b3e0fcb4c26cee52a6154
Jonathan Gulliksen <jonathangullik@gmail.com>
2022-03-18 09:29:20 +0100

    Added sorting for sauna data Fix #28

--- src/components/widgets/SaunaWidget/useGetSaunaData.ts
+++ src/components/widgets/SaunaWidget/useGetSaunaData.ts
@@ -8,7 +8,8 @@ export const useGetSaunaData = () => {
     fetch("https://api.bhd.v3p2k2.com/sauna/data")
       .then((res) => res.json())
       .then((data: SaunaResponseBody) => {
-        setSaunaData(data.data);
+        console.log(data.data);
+        setSaunaData(data.data.sort((a, b) => (a.id > b.id ? 1 : -1)));
       })
       .catch((err) => {
         console.error(err);
