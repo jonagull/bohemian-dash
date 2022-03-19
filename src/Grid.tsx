f7974a93c08a1b531d9a59267f207fb790cebd04
Jonathan Gulliksen <jonathangullik@gmail.com>
2022-03-19 18:22:00 +0100

    Turned header into resizable widget, made breakpoint to fit phone better

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from "react";
 import GridLayout from "react-grid-layout";
+import Header from "./components/Header";
 import { SaunaWidget } from "./components/widgets/SaunaWidget/SaunaWidget";
 import { StockWidget } from "./components/widgets/StockWidget";
 import WeatherWidget from "./components/widgets/WeatherWidget/WeatherWidget";
 
 export const Grid = () => {
   const layout = [
-    { i: "a", x: 0, y: 0, w: 3, h: 9 },
-    { i: "b", x: 3, y: 0, w: 3, h: 9 },
-    { i: "c", x: 6, y: 0, w: 3, h: 9 },
+    { i: "a", x: 8, y: 3, w: 8, h: 24 },
+    { i: "b", x: 0, y: 3, w: 4, h: 12 },
+    { i: "c", x: 0, y: 3, w: 4, h: 12 },
+    { i: "d", x: 0, y: 0, w: 12, h: 2 },
+  ];
+
+  const layoutPhone = [
+    { i: "a", x: 0, y: 20, w: 12, h: 8 },
+    { i: "b", x: 0, y: 12, w: 12, h: 8 },
+    { i: "c", x: 0, y: 3, w: 12, h: 8 },
+    { i: "d", x: 0, y: 0, w: 12, h: 2 },
   ];
 
   const [width, setWidth] = useState<number>(window.innerWidth);
@@ -24,10 +33,10 @@ export const Grid = () => {
   return (
     <GridLayout
       className="layout"
-      layout={layout}
+      layout={width < 800 ? layoutPhone : layout}
       cols={12}
       rowHeight={30}
-      width={width}
+      width={width - 35}
     >
       <div key="a">
         <StockWidget />
@@ -37,6 +46,9 @@ export const Grid = () => {
       </div>
       <div key="c">
         <SaunaWidget />
+      </div>
+      <div key="d">
+        <Header />
       </div>
     </GridLayout>
   );
