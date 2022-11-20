cda643e007d1748ed7bfac051fb3d749d27d00e7
jonagull <jonathangullik@gmail.com>
2022-11-20 22:33:14 +0100

    fixed typing, fixed rendering of correct data, added interval to change quote

--- src/components/widgets/StoicWidget/index.tsx
+++ src/components/widgets/StoicWidget/index.tsx
@@ -1,56 +1,70 @@
-import React, { useState } from "react";
-import { useGetStoicQuote } from "./useGetStoicQuote";
-import { ReactComponent as GreekLogo } from "../../../assets/icons/greek.svg";
-import { ReactComponent as CloudLogo } from "../../../assets/icons/cloud.svg";
-import { ReactComponent as CloudAltLogo } from "../../../assets/icons/cloud-alt.svg";
-
+import { CloudAltIcon, CloudIcon, GreekIcon } from "icons";
+import React, { useEffect, useState } from "react";
+import StoicQuotes from "./StoicQuotes.json";
 interface StoicData {
-  author: string;
-  quote: string;
+    text: string;
+    author: string;
+    book: string | null;
 }
 
 export const StoicWidget = () => {
-  const [stoicData, setStoicData] = useState<StoicData[] | undefined>([]);
-  useGetStoicQuote(setStoicData);
+    const filteredQuotes: StoicData[] = StoicQuotes.filter((x) => {
+        return x.text.length < 100;
+    });
 
-  const randomNumber = Math.floor(Math.random() * 10);
+    const randomNumberCreator = () => {
+        return Math.floor(Math.random() * filteredQuotes.length);
+    };
 
-  return (
-    <React.Fragment>
-      <div className="stoic-widget__wrapper">
-        <GreekLogo />
-        <div className="quote__container">
-          {stoicData && <p>"{stoicData[randomNumber]?.quote}"</p>}
-          {stoicData && <h6>- {stoicData[randomNumber]?.author}</h6>}
-        </div>
-        <div className="clouds__wrapper">
-          <div className="cloud-b__container">
-            <div className="inner-cloud-b__container">
-              <CloudAltLogo transform="scale (-1 1)" />
-            </div>
-            <div className="inner-cloud-a__container">
-              <CloudAltLogo transform="scale (-1 1)" />
-            </div>
-            <CloudLogo />
-            <div className="inner-cloud-c__container">
-              <CloudAltLogo transform="scale (-1 1)" />
-            </div>
-          </div>
+    const [randomNumber, setRandomNumber] = useState(1);
 
-          <div className="cloud-c__container">
-            <div className="inner-cloud-b__container">
-              <CloudAltLogo transform="scale (-1 1)" />
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setRandomNumber(randomNumberCreator());
+        }, 10000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <React.Fragment>
+            <div className="stoic-widget__wrapper">
+                <GreekIcon />
+                <div className="quote__container">
+                    {filteredQuotes && (
+                        <p>{filteredQuotes[randomNumber]?.text}</p>
+                    )}
+                    {filteredQuotes && (
+                        <h6>- {filteredQuotes[randomNumber]?.author}</h6>
+                    )}
+                </div>
+                <div className="clouds__wrapper">
+                    <div className="cloud-b__container">
+                        <div className="inner-cloud-b__container">
+                            <CloudAltIcon transform="scale (-1 1)" />
+                        </div>
+                        <div className="inner-cloud-a__container">
+                            <CloudAltIcon transform="scale (-1 1)" />
+                        </div>
+                        <CloudIcon />
+                        <div className="inner-cloud-c__container">
+                            <CloudAltIcon transform="scale (-1 1)" />
+                        </div>
+                    </div>
+
+                    <div className="cloud-c__container">
+                        <div className="inner-cloud-b__container">
+                            <CloudAltIcon transform="scale (-1 1)" />
+                        </div>
+                        <div className="inner-cloud-a__container">
+                            <CloudAltIcon transform="scale (-1 1)" />
+                        </div>
+                        <CloudIcon />
+                        <div className="inner-cloud-c__container">
+                            <CloudAltIcon transform="scale(0.7, -0.7) rotate(-30px)" />
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="inner-cloud-a__container">
-              <CloudAltLogo transform="scale (-1 1)" />
-            </div>
-            <CloudLogo />
-            <div className="inner-cloud-c__container">
-              <CloudAltLogo transform="scale(0.7, -0.7) rotate(-30px)" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </React.Fragment>
-  );
+        </React.Fragment>
+    );
 };
