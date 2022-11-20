061c1f519e7f9691e6a37a0511200a1d57b5347e
jonagull <jonathangullik@gmail.com>
2022-11-20 22:29:18 +0100

    formater

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -7,54 +7,54 @@ import { StockWidget } from "./components/widgets/StockWidget";
 import WeatherWidget from "./components/widgets/WeatherWidget/WeatherWidget";
 
 export const Grid = () => {
-  const layout = [
-    { i: "a", x: 8, y: 3, w: 8, h: 24 },
-    { i: "b", x: 0, y: 3, w: 4, h: 12 },
-    { i: "c", x: 0, y: 3, w: 4, h: 12 },
-    { i: "d", x: 0, y: 0, w: 12, h: 2 },
-    { i: "e", x: 0, y: 0, w: 4, h: 12 },
-  ];
+    const layout = [
+        { i: "a", x: 8, y: 3, w: 8, h: 24 },
+        { i: "b", x: 0, y: 3, w: 4, h: 12 },
+        { i: "c", x: 0, y: 3, w: 4, h: 12 },
+        { i: "d", x: 0, y: 0, w: 12, h: 2 },
+        { i: "e", x: 0, y: 0, w: 4, h: 12 },
+    ];
 
-  const layoutPhone = [
-    { i: "a", x: 0, y: 20, w: 12, h: 8 },
-    { i: "b", x: 0, y: 12, w: 12, h: 8 },
-    { i: "c", x: 0, y: 3, w: 12, h: 8 },
-    { i: "d", x: 0, y: 0, w: 12, h: 2 },
-  ];
+    const layoutPhone = [
+        { i: "a", x: 0, y: 20, w: 12, h: 8 },
+        { i: "b", x: 0, y: 12, w: 12, h: 8 },
+        { i: "c", x: 0, y: 3, w: 12, h: 8 },
+        { i: "d", x: 0, y: 0, w: 12, h: 2 },
+    ];
 
-  const [width, setWidth] = useState<number>(window.innerWidth);
+    const [width, setWidth] = useState<number>(window.innerWidth);
 
-  const updateInnerWidth = () => {
-    setWidth(window.innerWidth);
-  };
+    const updateInnerWidth = () => {
+        setWidth(window.innerWidth);
+    };
 
-  useEffect(() => {
-    window.addEventListener("resize", updateInnerWidth, false);
-  });
+    useEffect(() => {
+        window.addEventListener("resize", updateInnerWidth, false);
+    });
 
-  return (
-    <GridLayout
-      className="layout"
-      layout={width < 800 ? layoutPhone : layout}
-      cols={12}
-      rowHeight={30}
-      width={width - 35}
-    >
-      <div key="a">
-        <StockWidget />
-      </div>
-      <div key="b">
-        <WeatherWidget />
-      </div>
-      <div key="c">
-        <SaunaWidget />
-      </div>
-      <div key="d">
-        <Header />
-      </div>
-      <div key="e">
-        <StoicWidget />
-      </div>
-    </GridLayout>
-  );
+    return (
+        <GridLayout
+            className="layout"
+            layout={width < 800 ? layoutPhone : layout}
+            cols={12}
+            rowHeight={30}
+            width={width - 35}
+        >
+            <div key="a">
+                <StockWidget />
+            </div>
+            <div key="b">
+                <WeatherWidget />
+            </div>
+            <div key="c">
+                <SaunaWidget />
+            </div>
+            <div key="d">
+                <Header />
+            </div>
+            <div key="e">
+                <StoicWidget />
+            </div>
+        </GridLayout>
+    );
 };
