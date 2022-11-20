4a44f61eb4ab2fcaf5427bb330f9045344de1323
jonagull <jonathangullik@gmail.com>
2022-11-20 23:18:34 +0100

    exported icons from its own file

--- src/icons.ts
+++ src/icons.ts
@@ -1,1 +1,4 @@
 export { ReactComponent as FireIcon } from "assets/icons/fire.svg";
+export { ReactComponent as GreekIcon } from "./assets/icons/greek.svg";
+export { ReactComponent as CloudIcon } from "./assets/icons/cloud.svg";
+export { ReactComponent as CloudAltIcon } from "./assets/icons/cloud-alt.svg";
